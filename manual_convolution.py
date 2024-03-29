1b857b2afe5f6b34c819a2d70ba78d3187494a90
ale19 <ale.petru97@gmail.com>
2024-03-29 17:52:03 +0100

    checked against test set and tflite output, just rounding differences

--- manual_convolution.py
+++ manual_convolution.py
@@ -56,33 +56,40 @@ def quantize(array: np.ndarray):
             print("another array dtype:", array.dtype)
     return quantized
 
+filters = None
+biases = None
+weights = None
+bias = None
+
 def load_convolutional():
+    global filters, biases
+    if filters is not None and biases is not None:
+        return filters, biases
     filters = np.load("sequential_conv2d_Conv2D1.npy").astype(np.int8)
     biases = np.load("sequential_conv2d_Conv2D.npy").astype(np.int32)
-    print("filter info", filters.shape, filters.dtype)
-    print("bias info", biases.shape, biases.dtype)
+    # print("filter info", filters.shape, filters.dtype)
+    # print("bias info", biases.shape, biases.dtype)
+    # print(biases)
     return filters, biases
 
-# q_input = quantize(input)
-
-# weights_out = np.load("sequential_dense_MatMul.npy").astype(np.int8)
-# # print("WEIGHTS", weights_out.shape, weights_out.dtype)
-# bias_out = np.load("sequential_dense_BiasAdd_ReadVariableOp.npy").astype(np.int32)
-
-# filter, bias = load_convolutional()
-# # Precompute part
-# # qb − ZX qW
-# q_bias = bias_out - (weights_out @ (np.ones(shape=(weights_out.shape[1],), dtype=np.int32) * Z_input))
-# print(q_bias, q_bias.shape, q_bias.dtype)
-
-# # bias_out = np.reshape(bias_out, [10,1]).astype(np.int32)
-# # print("BIAS", bias_out)
-# # # print(bias_out.shape)
-# print(f"input {q_input.dtype} weights: {weights_out.dtype} bias: {bias_out.dtype}")
-# print(q_input.shape, weights_out.shape, bias_out.shape)
+def load_dense():
+    global weights, bias
+    if weights is not None and bias is not None:
+        return weights, bias
+    weights = np.load("sequential_dense_MatMul.npy").astype(np.int8)
+    bias = np.load("sequential_dense_BiasAdd_ReadVariableOp.npy").astype(np.int32)
+    # print("weights info", weights.shape, weights.dtype)
+    # print("bias info", bias.shape, bias.dtype)
+    return weights, bias
 
 # Do convolution
-def convolution(input: np.ndarray, filters: np.ndarray, q_bias: np.int32, scale: float, Zy: np.int8) -> np.ndarray:
+def convolution(input: np.ndarray, filters: np.ndarray, q_bias: np.int32) -> np.ndarray:
+    # Step 1.2: calculate the scaline factor for the convolutional layer
+    Sx = 1 / 255 # Input scaling factor
+    Sw = 0.008923129178583622 # Weight scaling factor
+    Sy = 0.02108168788254261 # Output scaling factor
+    scale_factor = (Sx * Sw) / Sy
+    Zy = -128 # Output zero point
     feature_maps = np.zeros(
         shape=(
             filters.shape[0], # same number as the number of filters
@@ -102,7 +109,7 @@ def convolution(input: np.ndarray, filters: np.ndarray, q_bias: np.int32, scale:
             feature_rows,
             feature_cols
         )).astype(np.int32)
-        print("out", out.shape, out.dtype)
+        # print("out", out.shape, out.dtype)
         # Convolution operation
         for i in range(feature_rows):
             for j in range(feature_cols):
@@ -111,7 +118,7 @@ def convolution(input: np.ndarray, filters: np.ndarray, q_bias: np.int32, scale:
                         out[i, j] += filter[ii, jj] * reshaped_input[i + ii, j + jj]
                 # Relu layer
                 out[i, j] = out[i, j] + q_bias
-                out[i, j] = scale * out[i, j]
+                out[i, j] = scale_factor * out[i, j]
                 out[i, j] += Zy
                 out[i, j] = max(out[i, j], -128)
                 out[i, j] = min(out[i, j], 127)
@@ -121,51 +128,40 @@ def convolution(input: np.ndarray, filters: np.ndarray, q_bias: np.int32, scale:
 
 # Calculate q_b_conv = q_b - conv(q_w, Zx)
 def conv(q_w: np.ndarray, Z_x: np.ndarray):
-    print(q_w.shape, Z_x.shape)
-    print(q_w)
-    print(Z_x)
+    # print(q_w.shape, Z_x.shape)
+    # print(q_w)
+    # print(Z_x)
     q_bias = np.int32(0)
     for i in range(q_w.shape[0]):
         for j in range(q_w.shape[1]):
             q_bias += np.int32(q_w[i][j]) * np.int32(Z_x[i][j])
     return q_bias
 
-# q_conv_bias = conv(filter.reshape(3,3), np.ones(shape=(filter.shape[1], filter.shape[2]), dtype=np.int32) * Z_input)
-# print(q_conv_bias)
-# f_map = convolution(q_input.astype(np.int32), filter.astype(np.int32), q_conv_bias)
-# print(f_map)
+def dense_layer(q_flat: np.ndarray, q_weights: np.ndarray, q_bias: np.ndarray) -> np.ndarray:
+    # XXX: This is a manual implementation of the dense layer, change if the network is trained again
+    S_x = 0.02108168788254261 # Input scaling factor dense
+    S_w = 0.004317202605307102 # Weight scaling factor dense
+    S_y = 0.12329018861055374 # Output scaling factor dense
+    Z_y = -19 # Output zero point dense
+    scale_factor_dense = (S_x * S_w) / S_y
 
-# flatten_input = q_input.flatten()
-# print(flatten_input.shape)
-# dotdot = weights_out.astype(np.int32) @ flatten_input.astype(np.int32)
-# print(dotdot, dotdot.shape, dotdot.dtype)
-# acc = dotdot + q_bias
-# acc = ((S_input*S_weight)/S_output) * acc
-# acc += Z_output
-# acc = np.rint(acc)
-# print(acc)
-# print(acc.astype(np.int8))
-
-def do_dot(q_flat: np.ndarray, q_weights: np.ndarray, q_bias: np.ndarray, scale: float, Zy: np.int8) -> np.ndarray:
     dot_prod = np.dot(q_weights.astype(np.int32), q_flat.astype(np.int32))
     acc = dot_prod + q_bias
-    acc = (scale * acc).astype(np.float32)
-    acc += Zy
+    acc = (scale_factor_dense * acc).astype(np.float32)
+    acc += Z_y
     acc = np.maximum(acc, -128)
     acc = np.minimum(acc, 127)
     acc = np.rint(acc) # round to the nearest integer
     acc = acc.astype(np.int8)
     return acc
 
-def evaluate_test_set(q_weights: np.ndarray, q_bias: np.ndarray):
+def evaluate_test_set():
     correct = 0
     for image, label in zip(x_test, y_test):
         # image = image / 255.0
-        image = np.expand_dims(image, axis=0) # from (28,28) to (1,28,28)
-        q_image = quantize(image)
-        q_flat = q_image.flatten()
-        acc = do_dot(q_flat=q_flat, q_weights=q_weights, q_bias=q_bias, scale=0.02108168788254261, Zy=-19)
-        predicted = np.argmax(acc)
+        image = np.expand_dims(image, axis=(0, -1)) # from (28,28) to (1,28,28)
+        ret = my_invoke(image)
+        predicted = np.argmax(ret)
         if label == predicted:
             correct += 1
     print("Lite accuracy:", (correct/10000)*100, "%")
@@ -178,7 +174,7 @@ def check_wrong(q_weights: np.ndarray, q_bias: np.ndarray):
     image = np.expand_dims(image, axis=0) # from (28,28) to (1,28,28)
     q_image = quantize(image)
     q_flat = q_image.flatten()
-    acc = do_dot(q_flat=q_flat, q_weights=q_weights, q_bias=q_bias, scale=0.02108168788254261, Zy=-19)
+    acc = dense_layer(q_flat=q_flat, q_weights=q_weights, q_bias=q_bias)
     predicted = np.argmax(acc)
     print(acc, predicted, label)
 
@@ -186,26 +182,41 @@ def check_wrong(q_weights: np.ndarray, q_bias: np.ndarray):
 
 def my_invoke(image: np.ndarray) -> np.ndarray:
     q_image = quantize(image)
-    q_flat = q_image.flatten()
-    # acc = do_dot(q_flat=q_flat, q_weights=weights_out, q_bias=bias_out)
-    return q_image # FIXME
+    filters, biases = load_convolutional()
+    Z_x = -128 # Input zero point conv
+    q_bias_conv = precompute_qbias(layer="conv", q_b=biases, q_w=filters, Z_x=Z_x)
+    q_conv = convolution(q_image.astype(np.int32), filters.astype(np.int32), q_bias_conv)
+    maxpooled = maxpool2D(q_conv)
+    q_weights, q_b_dense = load_dense()
+    Z_x = -128 # Input zero point dense
+    q_dense_bias = precompute_qbias(layer="dense", q_b=q_b_dense, q_w=q_weights, Z_x=Z_x)
+    result = dense_layer(q_flat=maxpooled.flatten(), q_weights=q_weights, q_bias=q_dense_bias)
+    return result
 
 
 def compare_results():
-    interpreter = tf.lite.Interpreter("./models/lite/int_model.tflite")
+    interpreter = tf.lite.Interpreter("./models/lite/int_cnn1d_conv_model.tflite")
     interpreter.allocate_tensors()
     idx = 0
     for image, label in zip(x_test, y_test):
-        image = np.expand_dims(image, axis=0)
+        image = np.expand_dims(image, axis=(0, -1))
+        # print(image.shape)
         interpreter.set_tensor(interpreter.get_input_details()[0]["index"], image)
         interpreter.invoke()
         ret = my_invoke(image)
-        out = interpreter.get_tensor(6)[0]
+        out = interpreter.get_tensor(10)[0]
+        if(np.argmax(ret) != np.argmax(out)):
+            print("idx:", idx, "My:", ret, "TFLite:", out, "MyPred:", np.argmax(ret), "TfPred:", np.argmax(out))
         if(np.array_equal(ret, out) == False):
             table.add_row([idx, ret, out, np.argmax(ret), np.argmax(out), label])
         idx += 1
-
-    print(table)
+    table_string = table.get_string()
+    num_rows = len(table_string.split("\n"))
+    if(num_rows > 40):
+        with open("table.txt", "w") as f:
+            f.write(table_string)
+    else:
+        print(table)
 
 def maxpool2D(f_maps: np.ndarray, kernel_size = (2, 2), stride = 2):
     h_in = f_maps[0].shape[0] # rows
@@ -240,6 +251,19 @@ def maxpool2D(f_maps: np.ndarray, kernel_size = (2, 2), stride = 2):
         maxpooled_features[f_map_idx] = out
     return maxpooled_features
 
+def precompute_qbias(layer:str, q_b: np.int32, q_w: np.ndarray, Z_x: np.int32):
+    if layer == "conv":
+        # q_bias = q_b - conv(q_w, Zx)
+        q_w = q_w.reshape(q_w.shape[1], q_w.shape[2])
+        q_bias = q_b - conv(q_w.astype(np.int32), np.ones(shape=(q_w.shape[0], q_w.shape[1]), dtype=np.int32) * Z_x)
+    elif layer == "dense":
+        # q_bias = q_b - dot(q_w, Zx)
+        q_bias = q_b - (q_w @ (np.ones(shape=(q_w.shape[1],), dtype=np.int32) * Z_x))
+    else:
+        raise ValueError("Invalid layer")
+    return q_bias
+
+
 def main():
     # Load the same input used in the tflite inference evaluation
     input = load_input()
@@ -251,43 +275,28 @@ def main():
     filters, biases = load_convolutional()
     print("filters meta:",filters.shape, filters.dtype)
     print("biases meta:",biases.shape, biases.dtype)
-    # Step 1.1: Calculate the q_bias for the convolutional layer
+    # Step 1.1: Precompute the q_bias for the convolutional layer
     # Y = Conv (W, X) + b 
     # with Zw=0 Zb=0 Sb=SwSx and q_bias = q_b - conv(q_w, Zx)
-    q_b = 0
-    q_w = filters.reshape(filters.shape[1], filters.shape[2])
-    Z_x = -128 # Input zero point
-    q_bias = q_b - conv(q_w.astype(np.int32), np.ones(shape=(q_w.shape[0], q_w.shape[1]), dtype=np.int32) * Z_x)
-    print("q_bias", q_bias)
-    # Step 1.2: calculate the scaline factor for the convolutional layer
-    Sx = 1 / 255 # Input scaling factor
-    Sw = 0.008923129178583622 # Weight scaling factor
-    Sy = 0.02108168788254261 # Output scaling factor
-    scale_factor = (Sx * Sw) / Sy
-    Zy = -128 # Output zero point
-    print("scale factor", scale_factor)
+    Z_x = -128 # Input zero point conv
+    q_bias_conv = precompute_qbias(layer="conv", q_b=biases, q_w=filters, Z_x=Z_x)
+    print("q_bias meta", q_bias_conv)
     # Step 2: Do the convolution
     # qY = (SwSx)/Sy (conv(qW, qX) + qBias) + Zy
-    q_conv = convolution(q_input.astype(np.int32), filters.astype(np.int32), q_bias, scale_factor, Zy)
+    q_conv = convolution(q_input.astype(np.int32), filters.astype(np.int32), q_bias_conv)
     print("q_conv meta:", q_conv.shape, q_conv.dtype)
-    print(q_conv)
     # Step 3: Maxpool 2D
     maxpooled = maxpool2D(q_conv)
     print("maxpooled meta:", maxpooled.shape, maxpooled.dtype)
-    print(maxpooled)
     # Step 4: Load the dense layer
-    q_weights = np.load("sequential_dense_MatMul.npy").astype(np.int8)
-    q_b_dense = np.load("sequential_dense_BiasAdd_ReadVariableOp.npy").astype(np.int32)
+    q_weights, q_b_dense = load_dense()
     Z_x = -128 # Input zero point dense
-    S_x = 0.02108168788254261 # Input scaling factor dense
-    S_w = 0.004317202605307102 # Weight scaling factor dense
-    S_y = 0.12329018861055374 # Output scaling factor dense
-    Z_y = -19 # Output zero point dense
-    q_dense_bias = q_b_dense - (q_weights @ (np.ones(shape=(q_weights.shape[1],), dtype=np.int32) * Z_x))
-    scale_factor_dense = (S_x * S_w) / S_y
-    result = do_dot(q_flat=maxpooled.flatten(), q_weights=q_weights, q_bias=q_dense_bias, scale=scale_factor_dense, Zy=Z_y)
+    q_dense_bias = precompute_qbias(layer="dense", q_b=q_b_dense, q_w=q_weights, Z_x=Z_x)
+    result = dense_layer(q_flat=maxpooled.flatten(), q_weights=q_weights, q_bias=q_dense_bias)
     print("result meta:", result.shape, result.dtype)
-    print(result)
+    print(result, np.argmax(result))
+    compare_results()
+    # evaluate_test_set()
 
 
 if __name__ == "__main__":
