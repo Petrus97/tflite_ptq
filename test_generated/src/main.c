6d0dac873ac385158e4ce07a18aa687c2926d02d
ale19 <ale.petru97@gmail.com>
2024-04-18 19:59:17 +0200

    manually adjusted main file

--- test_generated/src/main.c
+++ test_generated/src/main.c
@@ -5,7 +5,6 @@
 #include "model_opt.h"
 #endif
 
-
 #define BUF_SIZE(buf) sizeof(buf) / sizeof(buf[0])
 // Macro to define a generic print function for arrays
 #define PRINT_ARRAY(arr, size, formatSpecifier) \
@@ -34,9 +33,10 @@
         printf(" }\n");                              \
     } while (0)
 
-    int8_t feature_maps[1][26][26][2]
-    = { 0 };
-int8_t maxpooled[338] = { 0 };
+int8_t feature_maps_0[1][26][26][4] = { 0 };
+int8_t maxpooled_0[1][13][13][4] = {0};
+int8_t feature_maps_1[1][12][12][2] = {0};
+int8_t maxpooled[72] = { 0 };
 int8_t result[10];
 
 int main()
@@ -48,24 +48,30 @@ int main()
     };
     quantize(&image);
     // PRINT_ARRAY(image.i8, BUF_SIZE(image.i8), "%3d");
-    conv2d((int8_t(*)[28][28][1])image.i8, feature_maps);
+    conv2d_0((int8_t(*)[28][28][1])image.i8, feature_maps_0);
+    max_pool2d_0(feature_maps_0, maxpooled_0);
+    conv2d_1(maxpooled_0, feature_maps_1);
+    max_pool2d_1(feature_maps_1, (int8_t(*)[6][6][2])maxpooled);
+    fully_connected((int8_t(*)[72])maxpooled, result);
+    PRINT_ARRAY(result, 10, "%3d");
+    
 
-    for (size_t cout = 0; cout < 2; cout++) {
-        printf("{");
-        for (size_t i = 0; i < 26; i++) {
-            for (size_t j = 0; j < 26; j++) {
-                if (j + 1 == 26) {
-                    printf("%4d\n", feature_maps[0][i][j][cout]);
-                    continue;
-                }
-                printf("%4d ", feature_maps[0][i][j][cout]);
-            }
-        }
-        printf("}\n");
-    }
+    // for (size_t cout = 0; cout < 2; cout++) {
+    //     printf("{");
+    //     for (size_t i = 0; i < 26; i++) {
+    //         for (size_t j = 0; j < 26; j++) {
+    //             if (j + 1 == 26) {
+    //                 printf("%4d\n", feature_maps[0][i][j][cout]);
+    //                 continue;
+    //             }
+    //             printf("%4d ", feature_maps[0][i][j][cout]);
+    //         }
+    //     }
+    //     printf("}\n");
+    // }
 
-    max_pool2d(feature_maps, (int8_t(*)[13][13][2])maxpooled);
-    fully_connected((int8_t(*)[338])maxpooled, result);
-    PRINT_ARRAY(result, BUF_SIZE(result), "%3d");
+    // max_pool2d(feature_maps, (int8_t(*)[13][13][2])maxpooled);
+    // fully_connected((int8_t(*)[338])maxpooled, result);
+    // PRINT_ARRAY(result, BUF_SIZE(result), "%3d");
     return 0;
 }
